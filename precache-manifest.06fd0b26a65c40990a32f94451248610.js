8458ae37c5c53fb0544bf20451da8bb8df2f8736
u4bi <rootcode10@gmail.com>
2020-01-22 23:51:10 +0900

    Updates

---
+++ precache-manifest.06fd0b26a65c40990a32f94451248610.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1760782c1cbccca373d854223b0fd5",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.99217aee63bc695b967f.bundle.js"
+  },
+  {
+    "url": "runtime~main.99217aee63bc695b967f.bundle.js"
+  },
+  {
+    "url": "vendors~main.99217aee63bc695b967f.bundle.js"
+  },
+  {
+    "url": "vendors~main.99217aee63bc695b967f.bundle.js.LICENSE"
+  }
+]);
