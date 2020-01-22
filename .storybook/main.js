7714515d7fb73198468e30c9b8eb020ca79c30ea
u4bi <rootcode10@gmail.com>
2020-01-22 23:34:42 +0900

    chore: Initial react project using storybook.

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+    stories: [
+        '../src/**/*.stories.js',
+        '../src/**/*.stories.tsx'
+    ],
+    addons: [
+        '@storybook/preset-create-react-app',
+        '@storybook/addon-actions',
+        '@storybook/addon-links',
+    ]
+}
