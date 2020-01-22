d82fde8e70e5a631562a0aa46dcd1629d54eda5a
u4bi <rootcode10@gmail.com>
2020-01-23 00:34:47 +0900

    chore(storybook): Add @storybook/addon-storysource. - https://github.com/storybookjs/storybook/tree/master/addons/storysource

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
     stories: [
         '../src/**/*.stories.js',
@@ -7,5 +9,15 @@ module.exports = {
         '@storybook/preset-create-react-app',
         '@storybook/addon-actions',
         '@storybook/addon-links',
+        {
+            name: '@storybook/addon-storysource',
+            options: {
+                rule: {
+                    include: [
+                        path.resolve(__dirname, '../src/stories')
+                    ]
+                }
+            },
+        }
     ]
 }
